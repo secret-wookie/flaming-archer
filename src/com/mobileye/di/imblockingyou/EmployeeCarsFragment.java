c31aae08113821909b3eb1c823404eabe1986445
Dror Kronstein <dror.kronstein@mobileye.com>
2014-03-03 14:51:00 +0200

    Adding an initial list and making buttons react

--- src/com/mobileye/di/imblockingyou/EmployeeCarsFragment.java
+++ src/com/mobileye/di/imblockingyou/EmployeeCarsFragment.java
@@ -1,5 +1,8 @@
 package com.mobileye.di.imblockingyou;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,13 +24,50 @@ public class EmployeeCarsFragment extends Fragment {
 
 	public EmployeeCarsFragment() {
 	}
-
+	
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.wiki_view, container, false);
 		WebView wv = (WebView) rootView.findViewById(R.id.emplyee_cars_wiki);
+		InputStream is = getCurrentList();
+		
 		wv.setWebViewClient(new WebViewClient());
-		wv.loadUrl("http://wiki.me-corp.lan/mw/index.php/Employee_Cars_New");
+		String text = readListFile(is);
+		
+		wv.loadData(text, "text/html; charset=UTF-8", null);
 		return rootView;
+	}
+
+
+	private String readListFile(InputStream is) {
+		byte[] buf;
+		try {
+			buf = new byte[is.available()];
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			buf = new byte[128];
+		}
+		try {
+			is.read(buf);
+		} catch (IOException e) {
+			// TODO: We should probably do something here
+			e.printStackTrace();
+		}
+		return renderHtml(new String(buf));
+	}
+
+	private String renderHtml(String str) {
+		// Turns the list into an HTML string that can be viewed...
+		String res;
+		res = str.replace("border=&quot;1&quot; cellpadding=&quot;2&quot;", "").replace("{", "<table border=\"1\" cellpadding=\"2\"><tr>")
+				.replace("}", "</table>").replace("|-", "</tr><tr>").replace("|", "</td><td>");
+
+		return res;
+	}
+
+
+	private InputStream getCurrentList() {
+		return getResources().openRawResource(R.raw.initial_employee_list);
 	}
 }
--- src/com/mobileye/di/imblockingyou/OverviewSectionFragment.java
+++ src/com/mobileye/di/imblockingyou/OverviewSectionFragment.java
@@ -1,28 +1,55 @@
 package com.mobileye.di.imblockingyou;
 
+import com.mobileye.di.imblockingyou.OverviewActivity.SectionsPagerAdapter;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A dummy fragment representing a section of the app, but that simply
  * displays dummy text.
  */
 public class OverviewSectionFragment extends Fragment {
+	
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
 	/**
 	 * The fragment argument representing the section number for this
 	 * fragment.
 	 */
-	public static final String ARG_SECTION_NUMBER = "section_number";
-
 	public OverviewSectionFragment() {
+
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.overview_blocked_fragment, container, false);
+		
+		Button button = (Button) rootView.findViewById(R.id.add_blocked_button);		
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "add",  Toast.LENGTH_LONG).show();
+            }
+        });
+		button = (Button) rootView.findViewById(R.id.unblock_action_button);
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "unblock",  Toast.LENGTH_LONG).show();
+            }
+        });
+		
+		button = (Button) rootView.findViewById(R.id.block_action_button);
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "block",  Toast.LENGTH_LONG).show();
+            }
+        });
+		
 		return rootView;
 	}
 }
